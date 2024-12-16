9103f24da9f7693ad6817aa7ef7fd83a8b4c7f0b
Gan Kai <geeks.kai@gmail.com>
2024-12-16 11:58:21 +0800

    Update mode-toggle.tsx

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -2,18 +2,15 @@
 
 import * as React from "react";
 import { useTheme } from "next-themes";
-import Script from "next/script";
 
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
-declare var VConsole: any;
 
 export function ModeToggle() {
     const { theme, setTheme } = useTheme();
 
     return (
         <Button variant={"ghost"} onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        <Script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js" onReady={() => {new VConsole();}} />
             {theme === "dark" ? <SunIcon /> : <MoonIcon />}
         </Button>
     );
